295b65ea236f2158cccb17ad9328c2b2f1f7fea0
KennyC0412 <377545253@qq.com>
2021-02-03 20:05:46 +0800

    update

--- Linux/TcpClient/cmdThread.cpp
+++ Linux/TcpClient/cmdThread.cpp
@@ -1,25 +1,9 @@
 #define WIN32_LEAN_AND_MEAN
-#ifdef _WIN32
-	#include <windows.h>
-	#include <WinSock2.h>
-#else 
-	#include <unistd.h>
-	#include <arpa/inet.h>
-	#include <string.h>
-	#define SOCKET int
-	#define INVALID_SOCKET (SOCKET)(~0)
-	#define SOCKET_ERROR (-1)
-#endif
-
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <thread>
 #include "pre.h"
+#include "messageHeader.h"
+#include "client.h"
 
-bool RUN = true;
-
-void cmdThread(SOCKET c_sock)
+void cmdThread(TcpClient *client)
 {
 	
 	while (true) {
@@ -27,22 +11,22 @@ void cmdThread(SOCKET c_sock)
 		std::cin >> cmdBuf;
 		if (0 == strcmp(cmdBuf, "exit")) {
 			std::cout << "cmd thread quit." << std::endl;
-			RUN = false;
+			client->closeSocket();
 			break;
 		}
 		else if (0 == strcmp(cmdBuf, "login")) {
 			Login login;
 			strcpy(login.userName, "Kenny");
 			strcpy(login.passWord, "123456");
-			send(c_sock, (const char*)&login, sizeof(login), 0);
+			client->sendData(&login);
 		}
 		else if (0 == strcmp(cmdBuf, "logout")) {
 			Logout logout;
 			strcpy(logout.userName, "Kenny");
-			send(c_sock, (const char*)&logout, sizeof(logout), 0);
+			client->sendData(&logout);
 		}
 		else {
-			std::cout << "unknown command.please input again:";
+			std::cout << "Wrong command.please input again:";
 		}
 	}
 }
---
+++ Linux/TcpClient/main.cpp
@@ -0,0 +1,26 @@
+#define WIN32_LEAN_AND_MEAN
+#ifdef _WIN32
+#include <Windows.h>
+#include <WinSock2.h>
+#endif
+#include <iostream>
+#include <thread>
+#include "pre.h"
+#include "client.h"
+
+void cmdThread(TcpClient* client);
+
+int main()
+{
+	TcpClient client;
+	client.initSocket();
+	int retryTimes = 10;
+	while (client.connServer(net::IP, net::PORT) < 0 && retryTimes)
+		--retryTimes;
+	std::thread t(cmdThread, &client);
+	t.detach();
+	while (client.isRun() ) {
+		client.OnRun();
+	}
+	return 0;
+}
