c55d1d93aab7fe77c611d3a9eed67347fd30b8de
KennyC0412 <377545253@qq.com>
2021-02-25 17:03:20 +0800

    增加从命令行及配置文件读入配置的功能

--- Windows/TcpServer/TcpServer/main.cpp
+++ Windows/TcpServer/TcpServer/main.cpp
@@ -5,50 +5,27 @@
 #include "CellServer.h"
 #include "MyServer.hpp"
 #include "NetEnvMan.h"
-
-const char* argToStr(int argc, char* argv[], int index, const char* def, const char* argName)
-{
-	if (index >= argc) {
-		CELLLog_Error("argToStr, index=", index, "argv=", argv, argName);
-	}
-	else {
-		def = argv[index];
-	}
-	CELLLog_Info(argName, "=", def);
-	return def;
-}
-
-int  argToInt(int argc, char* argv[], int index, int def, const char* argName)
-{
-	if (index >= argc) {
-		CELLLog_Error("argToStr, index=", index, "argv=", argv, argName);
-	}
-	else {
-		def = atoi(argv[index]);
-	}
-	CELLLog_Info(argName, "=", def);
-	return def;
-}
+#include "CELLConfig.hpp"
 
 
 int main(int argc,char *argv[])
 {
-
-	//const char* strIP = argToStr(argc, argv, 1, "any", "strIP");
-	//uint16_t nPort = argToInt(argc, argv, 2,88, "nPort");
-	//int nThread = argToInt(argc, argv, 3, 4, "nThread");
-	//int nClient = argToInt(argc, argv, 4, 1000, "nClient");
-	//
-	//if (strcmp(strIP, "any") == 0) {
-	//	strIP = nullptr;
-	//}
+	CELLConfig::getInstance().Init(argc, argv);
+	const char* strIP = CELLConfig::getInstance().getCMD("strIP", "any");
+	uint16_t nPort = CELLConfig::getInstance().getCMD("nPort", 8888);
+	int nThread = CELLConfig::getInstance().getCMD("nThread", 4);
+	int nClient = CELLConfig::getInstance().getCMD("nClient", 1000);
+	
+	if (strcmp(strIP, "any") == 0) {
+		strIP = nullptr;
+	}
 
 	CELLLog::getInstance().setLogPath("temp\\serverLog", 'w');
     MyServer server;
     server.initSocket();
-    server.bindSocket(/*strIP,nPort*/);
+    server.bindSocket(strIP,nPort);
     server.listenPort();
-    server.Start(/*nThread*/);
+    server.Start(nThread);
 	while (true) {
 		char cmdBuf[256];
 		std::cin >> cmdBuf;
